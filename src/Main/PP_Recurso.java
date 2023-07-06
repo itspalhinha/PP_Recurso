f92f9e6c5c88bdec46729e6b2db27b957db36e1c
itspalhinha <rafacoronel8@hotmail.com>
2023-07-06 23:23:11 +0100

    rafa

--- src/Main/PP_Recurso.java
+++ src/Main/PP_Recurso.java
@@ -19,7 +19,8 @@ public class PP_Recurso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            //apresentar o register ou o login
+            
     }
     
 }
